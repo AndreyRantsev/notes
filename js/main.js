083f8a9e54598edbb957cc3dfb1a8dc137d7226c
Andrey Rantsev <Rantsev001@gmail.com>
2024-02-04 20:48:56 +0300

    refactor: changed function for render note

--- js/main.js
+++ js/main.js
@@ -6,18 +6,22 @@ const listNotes = document.querySelector('#listNotes');
 
 // =============Функции===================
 
-const prepareValue = (event) => {
-    event.preventDefault();
-    const addValue = inputAdd.value.trim();
-    if(addValue.length > 0) {
-        const note = 
+const renderNote = (value) => {
+    const note = 
         `
         <li class="list-notes__note">
-            <input type="text" class="input input--note" value=${addValue}>
+            <input type="text" class="input input--note" value=${value}>
             <button class="input input--del">Delete</button>
         </li>
         `;
         listNotes.insertAdjacentHTML("afterbegin", note);
+}
+
+const prepareValue = (event) => {
+    event.preventDefault();
+    const addValue = inputAdd.value.trim();
+    if(addValue.length > 0) {
+        renderNote(addValue);
     }
 }
 
