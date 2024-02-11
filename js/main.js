d9a5e8b55cb454ebcf6b812e9a5442c7e715747c
Andrey Rantsev <Rantsev001@gmail.com>
2024-02-11 20:20:04 +0300

    new feature: added function for pushing in local storage

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 const form = document.querySelector('#form');
 const inputAdd = form.querySelector('#inputAdd');
 const listNotes = document.querySelector('#listNotes');
+const allNotes = localStorage.getItem("allNotes") || [];
 
 // =============Функции===================
 
@@ -17,14 +18,41 @@ const renderNote = (value) => {
         listNotes.insertAdjacentHTML("afterbegin", note);
 }
 
+const pushNewNote = (value) => {
+    allNotes.push(value);
+}
+
 const prepareValue = (event) => {
     event.preventDefault();
     const addValue = inputAdd.value.trim();
     if(addValue.length > 0) {
+        const key = "allNotes";
         renderNote(addValue);
+        pushNewNote(addValue);
+        jsonItem({key, allNotes});
     }
 }
 
+const setItem = (object) => {
+    localStorage.setItem(object.key, object.allNotes);
+}
+
+const jsonItem = (object) => {
+    object.allNotes = JSON.stringify(object.allNotes);
+    setItem(object);
+}
 // =============События===================
 
 form.addEventListener('submit', prepareValue);
+
+//==Пример работы с локальным хранилищем==
+
+// const fruits = ["Банан", "Яблоко", "Авокадо"];
+// const fruitsJson = JSON.stringify(fruits);
+// localStorage.setItem("fruits", fruitsJson);
+
+// const fromLocal = localStorage.getItem("fruits");
+// console.log(fromLocal);
+// const fruitsParse = JSON.parse(fromLocal);
+// console.log(fruitsParse);
+
