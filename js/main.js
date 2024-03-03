2b916bd5d9fc81053baae43fffcc7e7f0a2b1e8b
Andrey Rantsev <Rantsev001@gmail.com>
2024-03-03 20:39:04 +0300

    feat: basic styles for button edit

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ const renderNote = (allNotes) => {
             const note = 
             `
             <li class="list-notes__note">
-            <div type="text" class="input input--note">${element}</div>
+            <textarea type="text" class="input input--note">${element}</textarea>
             <button class="input input--del">Delete</button>
             </li>
             `;
